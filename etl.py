d47fac1bd8c17b03887db23a81b3e99dc4db1c5e
Derryn Edwards <derryn_93@live.com>
2020-05-06 12:49:40 -0600

    replace user in db conn strings

--- etl.py
+++ etl.py
@@ -121,7 +121,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=dedwards")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=user")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
